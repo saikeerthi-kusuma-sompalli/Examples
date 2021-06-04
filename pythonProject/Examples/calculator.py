282a597c2e6f150ec828c17a5a8875beaceb3ce6
99004386-KusumaSompalli <kusuma.sompalli@ltts.com>
2021-06-04 12:17:27 +0530

    added code in calculator

---
+++ pythonProject/Examples/calculator.py
@@ -0,0 +1,39 @@
+"""
+This code is for calculator
+author: sompalli kusuma sai keerthi
+date: 01/06/2021
+time: 12:13 pm
+"""
+
+
+def add(num1, num2):
+    """
+
+    :param num1:
+    :param num2:
+    :return: sum of num1 and num2
+    """
+    return num1 + num2
+
+
+def sub(num1, num2):
+    """
+
+    :param num1:
+    :param num2:
+    :return: subtraction of num1 and num2
+    """
+    return num1 - num2
+
+
+def mul(num1, num2):
+    """
+
+    :param num1:
+    :param num2:
+    :return: Multiplication of num1 and num2
+    """
+    return num1 * num2
+
+
+print(add(10, 20))
